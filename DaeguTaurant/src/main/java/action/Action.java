3519a8832c6fe8677fee5282ae7e67e63a046522
zzsuzz <gkxm7qyf@naver.com>
2021-11-05 16:56:27 +0900

    test2추가수정

---
+++ DaeguTaurant/src/main/java/action/Action.java
@@ -0,0 +1,11 @@
+package action;
+//test2
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vo.ActionForward;
+
+public interface Action {
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+	throws Exception;
+}
