ca46d4e7bb7447e89915576d8b13e48b16a55f46
park-taerim <tgu05145@naver.com>
2021-11-25 17:39:54 +0900

    db 수정

--- DaeguTaurant/restaurant.sql
+++ DaeguTaurant/restaurant.sql
@@ -1,20 +1,21 @@
 create table restaurant(
 
-	res_id integer primary key,
-	res_name varchar2(45) not null,
-	res_score number(20) null,
-	res_readcount integer not null,
-	
-	res_category varchar2(15) not null,
-	res_mainmenu varchar2(300) not null,
-	res_number varchar2(15) not null,
-	res_Addr1 varchar2(9) not null,
-	res_Addr2 varchar2(50) not null,
-	res_hours varchar2(15) not null,
-	res_ref integer not null,
-	res_re_step integer not null,
-	res_notice_date timestamp(9) not null,
-	
+	res_id NUMBER primary key,				/*식당 고유번호 */
+	res_name varchar2(45) not null,			/*식당 이름 */
+	res_score NUMBER,						/*식당 평점 */
+	res_readcount NUMBER DEFAULT 0,			/*식당 조회수 */
+	res_category varchar2(15) not null,		/*식당 카테고리 */
+	res_mainmenu varchar2(300) not null,	/*식당 메뉴 */
+	res_number varchar2(15) not null,		/*식당 전화번호 */
+	res_Addr1 varchar2(9) not null,			/*식당 지역 */
+	res_Addr2 varchar2(50) not null,		/*식당 상세주소 */
+	res_hours varchar2(15) not null,		/*식당 영업시간 */
+	res_ref NUMBER not null,				/*식당 카테고리 번호 */
+	res_re_step NUMBER not null,			/*식당 맛집출력순서 */
+	res_notice_date TIMESTAMP(9) NOT null,	/*식당 등록시간 */
+	res_Addr1_ref NUMBER NOT NULL,			/*식당 지역 번호 */
+	res_pic varchar2(100) not null,			/*식당 이미지1 */
+	res_pic1 varchar2(100) not null,		/*식당 이미지2 */
 	res_user_id VARCHAR2(30),
 	FOREIGN KEY(res_user_id)
 	REFERENCES user_Info(user_id)
@@ -24,6 +25,7 @@ CREATE SEQUENCE restaurant_seq;
 /* sequence : 숫자 1씩 자동증가 기능*/
 
 drop table restaurant;
+drop SEQUENCE restaurant_seq;
 
 select * from RESTAURANT;
 
--- DaeguTaurant/user_Info.sql
+++ DaeguTaurant/user_Info.sql
@@ -18,3 +18,5 @@ update user_Info set user_level=9 where user_id='admin2@naver.com';
 update user_Info set user_level=9 where user_id='admin3@naver.com';
 update user_Info set user_level=9 where user_id='admin4@naver.com';
 
+
+
